eaf5149af16f56f28542c02788d189d25d728d48
Kilian Valdman <kilianvaldman@gmail.com>
2025-08-20 18:02:47 +0200

    cleanup

--- src/app/simple/page.tsx
+++
@@ -1,9 +0,0 @@
-import LessonLayout from '@/components/LessonLayout';
-import { getLessonById } from '@/lib/lessons-server';
-
-export default function SimplePage() {
-  // Use the first lesson as the simple page content
-  const lesson = getLessonById(1);
-
-  return <LessonLayout lesson={lesson} />;
-}
